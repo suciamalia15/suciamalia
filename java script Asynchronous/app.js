49a693744a95507efb4eba823b7a6fc05d119293
suciamalia15 <103485733+suciamalia15@users.noreply.github.com>
2024-10-23 09:54:35 +0700

    Delete java script Asynchronous directory

--- java script Asynchronous/app.js
+++
@@ -1,9 +0,0 @@
-console.log('Bakal muncul pertama');
-
-setTimeout(() => {
-    console.log(
-        'ini udah masuk memory proses tapi dijalankan setelah 3 detik kedepan'
-    );
-}, 3000);
-
-console.log('Bakal munculm kedua');
